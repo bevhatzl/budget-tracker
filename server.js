36cbc49202a3c223ac6e0cb906ae418f31ae395b
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-12-16 15:05:31 +1100

    working online only

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config();
 
 const PORT = 3000;
 
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const PWD = process.env.DB_PWD;
-const databaseUrl = `mongodb+srv://chick-flick:${encodeURIComponent(PWD)}@cluster0.fqf9a.mongodb.net/budget`;
+const databaseUrl = `mongodb+srv://chick-flick:${encodeURIComponent(PWD)}@cluster0.fqf9a.mongodb.net/budget-tracker`;
 
 // mongoose.connect("mongodb://localhost/budget", {
 //   useNewUrlParser: true,
