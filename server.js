6c4f3120b9cfc4aa0b882e3f6e0173031225490e
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-12-16 14:53:01 +1100

    mongodb connection

--- server.js
+++ server.js
@@ -12,13 +12,29 @@ app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+const PWD = process.env.DB_PWD;
+const databaseUrl = `mongodb+srv://chick-flick:${encodeURIComponent(PWD)}@cluster0.fqf9a.mongodb.net/budget`;
+
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+// Connection before deployment
+mongoose.connect(databaseUrl, { useNewUrlParser: true, useFindAndModify: false });
+
+// Connection for deployment on heroku
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/workout',
+//   {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false
+//   }
+// );
 
 // routes
 app.use(require("./routes/api.js"));
