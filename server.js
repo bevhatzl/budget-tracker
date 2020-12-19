d709c257cee326ccff624c92e592bdb0977a9c21
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-12-19 14:39:45 +1100

    mongoose connection for heroku deploy

--- server.js
+++ server.js
@@ -19,18 +19,18 @@ const PWD = process.env.DB_PWD;
 const databaseUrl = `mongodb+srv://chick-flick:${encodeURIComponent(PWD)}@cluster0.fqf9a.mongodb.net/budget-tracker`;
 
 // Connection before deployment
-mongoose.connect(databaseUrl, { useNewUrlParser: true, useFindAndModify: false });
+// mongoose.connect(databaseUrl, { useNewUrlParser: true, useFindAndModify: false });
 
 // Connection for deployment on heroku
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker',
-//   {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useCreateIndex: true,
-//       useFindAndModify: false
-//   }
-// );
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
